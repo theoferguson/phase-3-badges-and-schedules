7e6a045238c0bfdf1c7e7f00786e02a43afa96f1
Theo Ferguson <ferguson.theo@gmail.com>
2021-09-22 19:31:29 -0400

    commit tree c63a631877b69be9453b1b4c4e3d4d7329141ea6

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    name_array = []
+    names.each do |name|
+        name_array.push("Hello, my name is #{name}.")
+    end
+    return name_array
+end
+
+def assign_rooms(speakers)
+    speakers.map.with_index(1) do |speaker, index|
+        "Hello, #{speaker}! You'll be assigned to room #{index}!"
+    end
+end
+
+def printer(names)
+    batch_badge_creator(names).each do |name|
+        puts name
+    end
+    assign_rooms(names).each do |room|
+        puts room
+    end
+end
